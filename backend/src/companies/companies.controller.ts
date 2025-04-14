979ed28ecbc271d7f48fbfda1713bb06a221c30e
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-14 21:42:44 +0200

    Add Company API endpoints

---
+++ backend/src/companies/companies.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import { CompaniesService } from './companies.service';
+import { Company } from '@prisma/client';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('companies')
+@Controller('companies')
+export class CompaniesController {
+  constructor(private readonly companiesService: CompaniesService) {}
+
+  @Post()
+  create(@Body() createCompanyDto: CreateCompanyDto): Promise<Company> {
+    return this.companiesService.create(createCompanyDto);
+  }
+
+  @Get()
+  findAll(): Promise<Company[]> {
+    return this.companiesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Company | null> {
+    return this.companiesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCompanyDto: UpdateCompanyDto,
+  ): Promise<Company> {
+    return this.companiesService.update(id, updateCompanyDto);
+  }
+}
---
+++ backend/src/companies/companies.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/prisma/prisma.service';
+import { Company } from '@prisma/client';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+
+@Injectable()
+export class CompaniesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateCompanyDto): Promise<Company> {
+    return this.prisma.company.create({ data });
+  }
+
+  async findAll(): Promise<Company[]> {
+    return this.prisma.company.findMany();
+  }
+
+  async findOne(id: string): Promise<Company | null> {
+    return this.prisma.company.findUnique({ where: { id } });
+  }
+
+  async update(id: string, data: UpdateCompanyDto): Promise<Company> {
+    return this.prisma.company.update({ where: { id }, data });
+  }
+
+  async remove(id: string): Promise<Company> {
+    return this.prisma.company.delete({ where: { id } });
+  }
+}
