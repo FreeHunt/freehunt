2e20cb7c64c06461e647f91ba1c43b747e43567a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-15 21:19:52 +0200

    Fix and update PrismaServiceMock

--- backend/test/mocks/prisma.mock.ts
+++ backend/test/mocks/prisma.mock.ts
@@ -1,11 +1,30 @@
 export const PrismaServiceMock = {
-  user: [
-    {
-      create: jest.fn(),
-      findMany: jest.fn(),
-      findUnique: jest.fn(),
-      update: jest.fn(),
-      delete: jest.fn(),
-    },
-  ],
+  user: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
+  company: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
+  jobPosting: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
+  skill: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
 };
