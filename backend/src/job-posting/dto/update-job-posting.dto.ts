6c23a46df63ae95632936229dc212cc6731f778b
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-15 18:43:36 +0200

    Make Update DTOs partial type for User, Company and Job Posting

--- backend/src/job-posting/dto/update-job-posting.dto.ts
+++ backend/src/job-posting/dto/update-job-posting.dto.ts
@@ -1,3 +1,4 @@
 import { CreateJobPostingDto } from './create-job-posting.dto';
+import { PartialType } from '@nestjs/swagger';
 
-export class UpdateJobPostingDto extends CreateJobPostingDto {}
+export class UpdateJobPostingDto extends PartialType(CreateJobPostingDto) {}
