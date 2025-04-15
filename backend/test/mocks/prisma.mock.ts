1d5be88d90469405685aac20fe956980743339de
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-15 20:59:39 +0200

    Mock PrismaService

---
+++ backend/test/mocks/prisma.mock.ts
@@ -0,0 +1,11 @@
+export const PrismaServiceMock = {
+  user: [
+    {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  ],
+};
