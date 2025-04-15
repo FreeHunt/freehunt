0a1ecb5bb995db5cba74b0b238be10eae6ef0f64
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-15 07:18:16 +0200

    Add Skill DELETE endpoint

--- backend/src/skills/skills.controller.ts
+++ backend/src/skills/skills.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SkillsService } from './skills.service';
 import { Skill } from '@prisma/client';
 import { CreateSkillDto } from './dto/create-skill.dto';
@@ -31,5 +39,10 @@ export class SkillsController {
     @Body() updateSkillDto: UpdateSkillDto,
   ): Promise<Skill> {
     return this.skillsService.update(id, updateSkillDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<Skill> {
+    return this.skillsService.remove(id);
   }
 }
