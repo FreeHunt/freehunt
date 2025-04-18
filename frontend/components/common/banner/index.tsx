7414058e3cceb982b7728c966cd05d94f9adc049
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-18 21:12:35 +0200

    Add red-pointer.svg

--- frontend/components/common/banner/index.tsx
+++ frontend/components/common/banner/index.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
 function Banner({ text }: { text: string }) {
   return (
     <div className="hidden lg:flex bg-freehunt-beige-dark p-2.5 h-[200px] items-center justify-center">
-      <p className="text-freehunt-black-two text-[40px] font-bold w-[476px] text-center">
+      <p className="relative text-freehunt-black-two text-[40px] font-bold w-[476px] text-center">
         {text}
+        <Image
+          src="/assets/red-pointer.svg"
+          alt=""
+          className="absolute right-12 bottom-0"
+          width={67}
+          height={67}
+          loading="eager"
+        />
       </p>
     </div>
   );
