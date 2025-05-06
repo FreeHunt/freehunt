99910ecf23b817ad6996733692cc6cbce85f3a3b
cheick <cheicklanick@gmail.com>
2025-05-06 16:50:01 +0200

    Script Matomo

---
+++ frontend/components/MatomoScript.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function MatomoScript() {
+  return (
+    <Script id="matomo" strategy="afterInteractive">
+      {`
+        var _paq = window._paq = window._paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//localhost:8090/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      `}
+    </Script>
+  );
+}
