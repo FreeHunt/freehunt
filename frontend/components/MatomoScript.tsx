6a7dc3f557760c93ded81c11d4cb0a47a6600a59
cheick <cheicklanick@gmail.com>
2025-05-06 17:22:03 +0200

    update script url

--- frontend/components/MatomoScript.tsx
+++ frontend/components/MatomoScript.tsx
@@ -10,7 +10,7 @@ export default function MatomoScript() {
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
         (function() {
-          var u="//localhost:8090/";
+          var u="http://localhost:8090/";
           _paq.push(['setTrackerUrl', u+'matomo.php']);
           _paq.push(['setSiteId', '1']);
           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
