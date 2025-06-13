71febc165ecf709940c60575b092262387717e2e
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-06-13 19:26:40 +0200

    Add initial Terraform configuration for AWS S3 bucket in production

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project = "freehunt"
+      Owner   = "freehunt"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "freehunt_avatar" {
+  bucket        = var.freehunt_avatar_bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_cors_configuration" "freehunt_avatar_cors" {
+  bucket = aws_s3_bucket.freehunt_avatar.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = [var.frontend_url]
+  }
+}
